a36f5b2049829685be726cfb78417fd3272e1709
Michael Aufreiter <ma@zive.at>
2012-06-26 13:18:39 -0300

    Whitespace issues.

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/authenticate/:code', function(req, res) {
   });
 });
 
-var port = process.env.PORT || config.port || 9999;
+var port = process.env.PORT || config.port || 9999;
 
 app.listen(port, null, function (err) {
   console.log('Gatekeeper, at your service: http://localhost:' + port);
