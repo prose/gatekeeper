68236c64a5ae78aeac0712ec6e83cd21e0405602
Antoine RENELEAU <antoine@reneleau.fr>
2015-06-25 14:43:51 +0200

    add multiapp compatibility

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ var url     = require('url'),
     https   = require('https'),
     fs      = require('fs'),
     qs      = require('querystring'),
+    _       = require('lodash'),
     express = require('express'),
     app     = express();
 
 // Load config defaults from JSON file.
-// Environment variables override defaults.
 function loadConfig() {
   var config = JSON.parse(fs.readFileSync(__dirname+ '/config.json', 'utf-8'));
-  for (var i in config) {
-    config[i] = process.env[i.toUpperCase()] || config[i];
-  }
   console.log('Configuration');
   console.log(config);
   return config;
@@ -20,18 +17,24 @@ function loadConfig() {
 
 var config = loadConfig();
 
-function authenticate(code, cb) {
+function authenticate(clientId, code, cb) {
+  var appConfig = _.findWhere(config.apps, {'oauth_client_id': clientId});
+  if (!appConfig) {
+    cb('No app configurated for client ID : ' + clientId);
+  }
+  console.log('Authenticating for app :');
+  console.log(appConfig);
   var data = qs.stringify({
-    client_id: config.oauth_client_id,
-    client_secret: config.oauth_client_secret,
+    client_id: appConfig.oauth_client_id,
+    client_secret: appConfig.oauth_client_secret,
     code: code
   });
 
   var reqOptions = {
-    host: config.oauth_host,
-    port: config.oauth_port,
-    path: config.oauth_path,
-    method: config.oauth_method,
+    host: appConfig.oauth_host,
+    port: appConfig.oauth_port,
+    path: appConfig.oauth_path,
+    method: appConfig.oauth_method,
     headers: { 'content-length': data.length }
   };
 
@@ -52,16 +55,17 @@ function authenticate(code, cb) {
 
 // Convenience for allowing CORS on routes - GET only
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS'); 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
 
-app.get('/authenticate/:code', function(req, res) {
+app.get('/authenticate/:client/:code', function(req, res) {
+  console.log('client id:' + req.params.client);
   console.log('authenticating code:' + req.params.code);
-  authenticate(req.params.code, function(err, token) {
+  authenticate(req.params.client, req.params.code, function(err, token) {
     var result = err || !token ? {"error": "bad_code"} : { "token": token };
     console.log(result);
     res.json(result);
